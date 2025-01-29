013f0ec9c80190f16bf6cbb6358be132a5287cfc
kyawzinhtet <kyawe225@gmail.com>
2025-01-29 12:05:41 +0700

    initial commit

---
+++ database/seeders/Teachers.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class Teachers extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
