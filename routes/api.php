66dea55cc5052faa7760291137b169e8021dbe7e
Myat Thuta <myatthuta232@gmail.com>
2025-02-25 22:56:41 +0630

    Blog Finish

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\ReAllocateController;
+use App\Http\Controllers\AllocationController;
 use App\Http\Controllers\BulkAllocationController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CommentController;
-use App\Http\Controllers\AllocationController;
-use App\Http\Controllers\Admin\ReAllocateController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -28,12 +28,19 @@ Route::middleware('auth:sanctum')->group(function () {
     // show non-allocated student
     Route::get('student/nonallocate', [BulkAllocationController::class, 'nonAllocatedStudentList']);
     // end non-allocated student
+
+                                                                                           //Blog Routes start
+    Route::get('/blogs/{id}', [App\Http\Controllers\BlogController::class, 'show']);       //---Show single blog---//
+    Route::post('/blogs', [App\Http\Controllers\BlogController::class, 'store']);          ///--Blog Create---//
+    Route::put('/blogs/{id}', [App\Http\Controllers\BlogController::class, 'update']);     //----Blog update---//
+    Route::delete('/blogs/{id}', [App\Http\Controllers\BlogController::class, 'destroy']); ///----Blog delete---//
+                                                                                           //////End of blog routes//////
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/comments', [CommentController::class, 'index']);     // List all comments
-    Route::post('/comments', [CommentController::class, 'store']);   // Add a comment
-    Route::put('/comments/{id}', [CommentController::class, 'update']); // Update a comment
-    Route::get('/comments/{id}', [CommentController::class, 'show']); // Get a single comment
+    Route::get('/comments', [CommentController::class, 'index']);           // List all comments
+    Route::post('/comments', [CommentController::class, 'store']);          // Add a comment
+    Route::put('/comments/{id}', [CommentController::class, 'update']);     // Update a comment
+    Route::get('/comments/{id}', [CommentController::class, 'show']);       // Get a single comment
     Route::delete('/comments/{id}', [CommentController::class, 'destroy']); // Delete a comment
 });
