c546e6696461cb40ff3327602c3d3cdb11feaae4
Myat Thuta <myatthuta232@gmail.com>
2025-03-08 23:04:27 +0630

    Student Seeder update

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use App\Models\Student;
 use Carbon\Carbon;
+use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class StudentSeeder extends Seeder
@@ -12,51 +13,22 @@ class StudentSeeder extends Seeder
         // Truncate table before seeding to avoid duplicates
         Student::truncate();
 
-        $students = [
-            [
-                "StudentID"    => "STD01",
-                "name"         => "Doris Navarro",
-                "email"        => "dorisst@gmail.com",
-                "password"     => bcrypt("password"), // Hash passwords
-                "phone_number" => "09790000000",
-            ],
-            [
-                "StudentID"    => "STD02",
-                "name"         => "Joanne Duke",
-                "email"        => "joannest@gmail.com",
-                "password"     => bcrypt("password"),
-                "phone_number" => "09790000004",
-            ],
-            [
-                "StudentID"    => "STD03",
-                "name"         => "Alden Beck",
-                "email"        => "dorissta@gmail.com",
-                "password"     => bcrypt("password"),
-                "phone_number" => "09790000003",
-            ],
-            [
-                "StudentID"    => "STD04",
-                "name"         => "Juanita Baird",
-                "email"        => "juanitast@gmail.com",
-                "password"     => bcrypt("password"),
-                "phone_number" => "09790000001",
-            ],
-            [
-                "StudentID"    => "STD05",
-                "name"         => "Wallace Cowan",
-                "email"        => "wallacest@gmail.com",
-                "password"     => bcrypt("password"),
-                "phone_number" => "09790000002",
-            ],
-        ];
+        $faker = Faker::create();
 
-        foreach ($students as $studentData) {
-            $studentData['created_at'] = Carbon::now('UTC');
-            $studentData['updated_at'] = Carbon::now('UTC');
+        for ($i = 1; $i <= 100; $i++) {
+            $studentData = [
+                "StudentID"    => sprintf("STD%03d", $i), // e.g. STD001, STD002, ...
+                "name"         => $faker->name,
+                "email"        => $faker->unique()->safeEmail,
+                "password"     => bcrypt("password"),              // Hash password using bcrypt
+                "phone_number" => $faker->numerify('097900#####'), // Generates a fake phone number
+                "created_at"   => Carbon::now('UTC'),
+                "updated_at"   => Carbon::now('UTC'),
+            ];
 
             $student = Student::create($studentData);
 
-            // Assign the "student" role (optional, only if using Spatie roles)
+            // Optionally assign the "student" role if using Spatie roles
             if (method_exists($student, 'assignRole')) {
                 $student->assignRole('student');
             }
