7925e5fe3341960a1bfebab3078f0eb4d65c8702
Myat Thuta <myatthuta232@gmail.com>
2025-03-08 13:05:38 +0630

    Student migration file changes

--- database/migrations/2025_02_15_005224_create_students_table.php
+++ database/migrations/2025_02_15_005224_create_students_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('StudentID');
             $table->string("name");
             $table->string("email")->unique();
             $table->string("password");
