b3640b62e9e3d5116178fbcc8bc7348828b37a8e
BhonePhilip <bhone2k18@gmail.com>
2025-03-21 14:44:13 +0630

    arranging fix store update

--- database/migrations/2025_03_05_132255_create_meeting_detail_table.php
+++ database/migrations/2025_03_05_132255_create_meeting_detail_table.php
@@ -14,18 +14,17 @@ return new class extends Migration
         Schema::create('meeting_detail', function (Blueprint $table) {
             $table->id();
             $table->timestampTz("arrange_date");
-            $table->enum("meeting_type", ['online', "offline"]);
-            $table->string("location")->nullable();
-            $table->string("meeting_link")->nullable();
-            $table->string("online_meeting_application_type"); // example : zoom , whatapps, teams , apple
+            $table->enum("meeting_type", ['online', 'offline']);
+            $table->string("location")->nullable(); // Nullable for online meetings
+            $table->string("meeting_link")->nullable(); // Nullable for physical meetings
+            $table->string("online_meeting_application_type")->nullable(); // Example: Zoom, Teams, WhatsApp
             $table->unsignedBigInteger("arrange_id");
             $table->foreign("arrange_id")
-                ->references('id')->on('arrangings')
-                ->onDelete('cascade');
-
-
+                  ->references('id')->on('arrangings')
+                  ->cascadeOnDelete();
             $table->string('topic');
-            $table->enum("status", ["cancelled", "pending", "completed", "reschelduled"]);
+            $table->text("description")->nullable(); // New column for description
+            $table->enum("status", ["cancelled", "pending", "completed", "rescheduled"])->default("pending");
             $table->softDeletesTz();
             $table->timestampsTz();
         });
